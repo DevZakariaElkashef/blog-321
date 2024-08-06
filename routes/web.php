076e26f3357d79a611ad076621d7489b4a70da77
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2024-08-06 21:13:52 +0300

    'star'

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Site\AboutController;
+use App\Http\Controllers\Site\ContactController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('about', [AboutController::class, 'index'])->name('about');
+
+Route::get('posts', [PostController::class, 'index'])->name('posts');
+
+Route::get('contact', [ContactController::class, 'index'])->name('contact');
