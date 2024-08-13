61313d1f8e04e0498e926dcbf827a9de28621946
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2024-08-13 21:48:50 +0300

    add dashboard to blog site

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Site\AboutController;
 use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +19,8 @@ use App\Http\Controllers\Site\PostController;
 |
 */
 
+
+Route::view('/home', 'welcome');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('about', [AboutController::class, 'index'])->name('about');
@@ -26,4 +30,24 @@ Route::get('posts', [PostController::class, 'index'])->name('posts');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 
 
-Route::view('/home', 'welcome');
+
+
+/*
+|--------------------------------------------------------------------------
+| admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::prefix('admin')->group(function() {
+
+  Route::get('/home', [AdminHomeController::class, 'index'])->name('admin.home');
+  Route::resource('users', UserController::class);
+
+});
+
